cbd49380f0cc9955fe70cbc2a2590d63a28a0cd2
Felix Kirchweger <fkirchw@gmail.com>
2018-01-12 15:05:59 +0100

    Dashboard initial commit

---
+++ public/JSCRIPT/dashboard.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    console.log("document.ready()");
+    $("#expand_custom_options").click(function (e) {
+        console.log("onclick");
+        $("#expand_custom_options, #expand_custom_options img").animate({ width:'300px' });
+        e.stopPropagation();
+    });
+    $("*:not(#expand_custom_options, #expand_custom_options img)").click(function (e) {
+        $("#expand_custom_options").style.width()
+        e.stopPropagation();
+    })
+});
