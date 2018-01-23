4dcc8380b1267678748da5d00ca8055afe2e321f
Felix Kirchweger <fkirchw@gmail.com>
2018-01-23 13:39:21 +0100

    Final Commit Dashboard

--- public/JSCRIPT/dashboard.js
+++ public/JSCRIPT/dashboard.js
@@ -1,12 +1,48 @@
 ﻿$(document).ready(function () {
+    $("#expand_custom_options")[0].style.width = '20%';
+    $("#main aside")[0].style.width = $("#expand_custom_options")[0].style.width;
     console.log("document.ready()");
     $("#expand_custom_options").click(function (e) {
-        console.log("onclick");
-        $("#expand_custom_options, #expand_custom_options img").animate({ width:'300px' });
+        $("#expand_custom_options")[0].style.width = '100%';
+        $("#main aside")[0].style.width = '100%';
+        $("#main aside")[0].style.alignItems = 'baseline';
+        $("#main aside")[0].style.minWidth = 'auto';
+        console.log(new Date().getMilliseconds());
+        setTimeout(function (e) {
+            console.log(new Date().getMilliseconds());
+            $("#main aside a span").toArray().forEach(function (e) {
+                console.log(e);
+                e.style.display = "inline-block";
+
+            })
+        }, 300);
+        
+        console.log("onExpandClick - " + parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width));
         e.stopPropagation();
     });
     $("*:not(#expand_custom_options, #expand_custom_options img)").click(function (e) {
-        $("#expand_custom_options").style.width()
+        $("#expand_custom_options")[0].style.width = '20%';
+        $("#main aside")[0].style.width = '20%';
+        $("#main aside")[0].style.alignItems = 'center';
+        $("#main aside")[0].style.minWidth = 'fit-content';
+        $("#main aside a span").toArray().forEach(function (e) {
+            console.log(e);
+            e.style.display = "none";
+        })
+        console.log("onShrinkClick");
         e.stopPropagation();
     })
+
+    //$("#expand_custom_options").click(function (e) {
+    //    $("#expand_custom_options")[0].style.width = '100%';
+    //    $("#main aside")[0].style.width = parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width);
+    //    console.log("onExpandClick - " + parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width));
+    //    e.stopPropagation();
+    //});
+    //$("*:not(#expand_custom_options, #expand_custom_options img)").click(function (e) {
+    //    $("#expand_custom_options")[0].style.width = '20%';
+    //    $("#main aside")[0].style.width = parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width);
+    //    console.log("onShrinkClick - " + parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width));
+    //    e.stopPropagation();
+    //})
 });
