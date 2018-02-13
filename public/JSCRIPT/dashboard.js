cc1033f2510048786a2ce30869812f6dd92b8928
Felix Kirchweger <fkirchw@gmail.com>
2018-02-13 11:00:47 +0100

    Ordnerstruktur, Dashboard.html

--- public/JSCRIPT/dashboard.js
+++ public/JSCRIPT/dashboard.js
@@ -32,17 +32,36 @@
         console.log("onShrinkClick");
         e.stopPropagation();
     })
-
-    //$("#expand_custom_options").click(function (e) {
-    //    $("#expand_custom_options")[0].style.width = '100%';
-    //    $("#main aside")[0].style.width = parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width);
-    //    console.log("onExpandClick - " + parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width));
-    //    e.stopPropagation();
-    //});
-    //$("*:not(#expand_custom_options, #expand_custom_options img)").click(function (e) {
-    //    $("#expand_custom_options")[0].style.width = '20%';
-    //    $("#main aside")[0].style.width = parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width);
-    //    console.log("onShrinkClick - " + parseFloat(window.getComputedStyle($("#expand_custom_options")[0]).width));
-    //    e.stopPropagation();
-    //})
+    $("#newTodo").click(function (e) {
+        var txt = $("#input_TODO")[0].value;
+        alert(txt);
+        
+    });
+    
+    $("#calendar").fullCalendar({
+        locale: 'de-at',
+        header: {
+            left: "prev next",
+            center: "today title",
+            height: "parent",
+            right: "month ,agendaWeek , agendaDay , listWeek"
+        },
+        editable:true,
+        events: [
+            {
+                "title": "Ceramics",
+                "id": "821",
+                "start": "2018-01-13 09:00:00",
+                "end": "2018-01-31 10:30:00"
+            },
+            {
+                "title": "Zippy",
+                "id": "822",
+                "start": "2018-01-31 10:00:00",
+                "end": "2018-01-31 11:30:00"
+            }
+        ]
+        });
+    
 });
+document.onload(alert("This website is not yet functional. All data displayed are dummy data"));
