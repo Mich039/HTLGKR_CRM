7f49818b6dc22f8181af6885ad46578250e173d9
Icecon <pascalwindhager@gmx.at>
2018-03-06 15:43:04 +0100

    87_add_appointment neu implementiert

---
+++ public/JSCRIPT/87_add_appointment.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    function appointment(appointmentName, invitedMembers, startDate, endDate, location, description, details) {
+        self = this
+        self.appointmentName = appointmentName;
+        self.invitedMembers = invitedMembers;
+        self.startDate = startDate;
+        self.endDate = endDate;
+        self.location = location;
+        self.description = description;
+        self.details = details;
+    }
+
+    function AddAppointmentModel() {
+
+        function addAppointmentClicked() {
+            var appointment = appointment();
+            var jsonString = ko.toJSON();
+        }
+    }
+
+    ko.applyBindings(new AddAppointmentModel());
+})
